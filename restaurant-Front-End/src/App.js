2b3742f2c895f81c923f6fbcf973bc6efc80c0a0
JALIGI LAXMAN KUMAR <laxmanjaligi@gmail.com>
2025-12-31 20:16:20 +0530

    fixed

--- restaurant-Front-End/src/App.js
+++ restaurant-Front-End/src/App.js
@@ -6,9 +6,8 @@ import SummaryPage from "./pages/SummaryPage";
 const router = createBrowserRouter([
   {path: "/", element: <HomePage restaurantName={" HOTEL ALPHA "}/>},
   {path: "/:tableId", element: <TablePage />},
-  {path: "/summary", element: <SummaryPage />},
- 
-], {basename: "/food-repo"});
+  {path: "/summary", element: <SummaryPage />}
+])
 
 function App() {
   return <RouterProvider router={router}/>;
